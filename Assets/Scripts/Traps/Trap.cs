602eb618ccdf7c332e143764d27369da49d8b58e
doCent90 <78255120+doCent90@users.noreply.github.com>
2022-04-28 10:25:54 +0300

    Update Trap.cs

--- Assets/Scripts/Traps/Trap.cs
+++ Assets/Scripts/Traps/Trap.cs
@@ -8,7 +8,7 @@ public class Trap : MonoBehaviour
     private RoadMover _splineMover;
     private BoxCollider _boxCollider;
 
-    private const float Delay = 2f;
+    private const float Delay = 0.5f;
 
     private void OnEnable()
     {
