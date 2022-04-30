78d96c201f00f2f41e1b0bb30882d8c937028fa0
doCent90 <78255120+doCent90@users.noreply.github.com>
2022-04-30 11:24:10 +0300

    Added to 15 lvls

--- Assets/Scripts/Levels/LevelsLoader.cs
+++ Assets/Scripts/Levels/LevelsLoader.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using IJunior.TypedScenes;
-using Random = UnityEngine.Random;
 
 public class LevelsLoader : MonoBehaviour, ISceneLoadHandler<int>
 {
@@ -9,7 +8,6 @@ public class LevelsLoader : MonoBehaviour, ISceneLoadHandler<int>
     [SerializeField] private bool _isStartApp = false;
     [SerializeField] private bool _isTestLevel = false;
 
-    private readonly int _levelsAmount = 3;
     private float _spentTime;
     private int _levelIndex;
 
@@ -68,47 +66,9 @@ public class LevelsLoader : MonoBehaviour, ISceneLoadHandler<int>
         _gameOver.Won -= OnLevelDone;
     }
 
-    private void RandomLevel()
-    {
-        int randomLevel = Random.Range(3, _levelsAmount + 1);
-        Load(randomLevel);
-    }
-
     private void Load(int number)
     {
-        switch (number)
-        {
-            case 1:
-                LVL_1.Load(number);
-                break;
-            case 2:
-                LVL_2.Load(number);
-                break;
-            case 3:
-                LVL_3.Load(number);
-                break;
-            case 4:
-                LVL_4.Load(number);
-                break;
-            case 5:
-                LVL_5.Load(number);
-                break;
-            case 6:
-                LVL_6.Load(number);
-                break;
-            case 7:
-                LVL_7.Load(number);
-                break;
-            case 8:
-                LVL_8.Load(number);
-                break;
-            case 9:
-                LVL_9.Load(number);
-                break;
-            default:
-                RandomLevel();
-                break;
-        }
+        LevelsStock.LoadLevel(number);
     }
 
     public void OnSceneLoaded(int level)
