30d996af2ad4b09ceb5c7bdba8f14e779290c595
doCent90 <78255120+doCent90@users.noreply.github.com>
2022-05-11 09:15:38 +0300

    Corrected

--- Assets/Scripts/Boss/ParticlesSwitcher.cs
+++ Assets/Scripts/Boss/ParticlesSwitcher.cs
@@ -5,20 +5,35 @@ public class ParticlesSwitcher : MonoBehaviour
 {
     [SerializeField] private GameOver _gameOver;
     [SerializeField] private ParticleSystem[] _finishParticleSystems;
+    [SerializeField] private ParticleSystem[] _fireWorksParticleSystems;
+
+    private const float Delay = 2f;
 
     private void OnEnable()
     {
-        _gameOver.Won += PlayFinishFX;
+        _gameOver.Won += PlayFireWorks;
     }
 
     private void OnDisable()
     {
-        _gameOver.Won -= PlayFinishFX;
+        _gameOver.Won -= PlayFireWorks;
+    }
+
+    private void PlayFireWorks()
+    {
+        PlayFX(_fireWorksParticleSystems);
+        Invoke(nameof(PlayFinishFX), Delay);
     }
 
     private void PlayFinishFX()
     {
-        foreach (ParticleSystem particle in _finishParticleSystems)
+        PlayFX(_finishParticleSystems);
+    }
+
+    private void PlayFX(ParticleSystem[] particles)
+    {
+        foreach (ParticleSystem particle in particles)
             particle.Play();
+
     }
 }
